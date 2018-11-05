74edc8b733927c57151ef0518dc13a6321a71051
KEITA TAIRA <kt10580@hotmail.co.jp>
2018-11-05 15:37:43 +0900

    devise機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181104104322) do
+ActiveRecord::Schema.define(version: 20181105042812) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -21,5 +21,19 @@ ActiveRecord::Schema.define(version: 20181104104322) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "nickname",               limit: 255
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end

d1d1715226c8a337c57a3939e397f9add90d3219
KEITA TAIRA <kt10580@hotmail.co.jp>
2018-11-05 12:00:43 +0900

    投稿画面実装(1)

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,4 +3,18 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    Blog.create(blog_params)
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :text, :image)
+  end
+
 end

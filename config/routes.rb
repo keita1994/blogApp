6d0a6373ceff2b62a342a0fbe09b8e87e2b0de02
KEITA TAIRA <kt10580@hotmail.co.jp>
2018-11-08 14:46:18 +0900

    Edit view 完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "blogs#index"
-  get  "blogs"       =>  "blogs#index"
-  get  "blogs/new"   =>  "blogs#new"
-  post  "blogs"      =>  "blogs#create"
-  get  "blogs/:id"   =>  "blogs#show"
-  patch "blogs/:id"  =>  "blog#update"
-  get "blogs/:id/edit"  =>  "blogs#edit"
-  delete "blogs/:id"  =>  "blogs#destroy"
-  get  "users/:id"    =>  "users#show"
+  root   "blogs#index"
+  get    "blogs"           =>    "blogs#index"
+  resources :blogs
+  get    "users/:id"       =>    "users#show"
 end
